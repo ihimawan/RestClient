d65073ec0e3acfb87a6674a3b37b58a139ead26b
ihimawan <indri.himawan18@yahoo.com>
2017-04-05 15:35:13 -0400

    added logging on client

--- ConsumeRest/src/main/java/com/consume/client/ClientService.java
+++ ConsumeRest/src/main/java/com/consume/client/ClientService.java
@@ -7,12 +7,17 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.consume.dto.Response;
 import com.consume.dto.State;
 import com.google.gson.Gson;
 
 public class ClientService {
-
+	
+	private static Logger log = LogManager.getLogger(ClientService.class);;
+	
 	public static List<State> getStates(String url) {
 		
 		//create client
@@ -24,11 +29,13 @@ public class ClientService {
 		//get response
 		String JSONResponseString = target.request(MediaType.APPLICATION_JSON).get(String.class);
 		
+		log.debug("Response is received.");
+		
 		//using Gson to parse JSON data.
 		Gson gson = new Gson();
 		
 		Response response = gson.fromJson(JSONResponseString, Response.class);
-				
+						
 		return response.getRestResponse().getResult();
 
 	}
