1bbb41ab58ed2f32bb7eadf187b07544cdca0805
ihimawan <indri.himawan18@yahoo.com>
2017-04-05 15:41:24 -0400

    added logging

--- ConsumeRest/src/main/java/com/consume/client/ClientService.java
+++ ConsumeRest/src/main/java/com/consume/client/ClientService.java
@@ -1,6 +1,8 @@
 package com.consume.client;
 
 import java.util.List;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -12,8 +14,12 @@ import com.consume.dto.State;
 import com.google.gson.Gson;
 
 public class ClientService {
+	
+	private static Logger log = Logger.getLogger(ClientService.class.getName());
 
 	public static List<State> getStates(String url) {
+		
+		log.info("something here");
 		
 		//create client
 		Client client = ClientBuilder.newClient();
